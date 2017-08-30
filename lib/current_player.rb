ff50bb0641963c2b7a33db0b96bae74fa97c854f
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-30 18:32:51 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |pos|
+    if pos == "X" || pos == "O"
+      count += 1
+  end
+end
+count
+end
+
+def current_player(board)
+  count = 1
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
